00af640323bf3b47b35df5332a06b5ea287173fc
harshalnmims <harhal.nanekar.EXT@nmims.edu>
2026-08-17 22:13:30 +0530

    Updated CI workflow and added build command

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 const app = require('express')();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!', process.env.NODE_ENV);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  res.send('Hello World!');
 });
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} ${process.env.NODE_ENV}`);
 });
