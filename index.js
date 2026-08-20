ecdc2bc82a17f828068d89412bcfe483d44cb864
harshalnmims <harhal.nanekar.EXT@nmims.edu>
2026-08-20 21:39:23 +0530

    added secrets in ci pipeline

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} ${process.env.NODE_ENV}`);
+  console.log(`Server is running on port ${PORT} ${process.env.NODE_ENV} ${process.env.API_SECRET}`);
 });
